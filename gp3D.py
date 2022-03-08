d9470e8a6296daa9d4f59a9c9a76cd2714b094bf
Matteo Perotto <perotto.matteo@gmail.com>
2022-03-08 16:13:17 +0100

    Experiments on superquadrics

---
+++ gp3D.py
@@ -0,0 +1,88 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as pl
+import math 
+
+
+# Define a superquadric
+N = 20
+ni = np.linspace(-math.pi()/2, math.pi()/2, N)
+omega = np.linspace(-math.pi(), math.pi(), N)
+
+eps1= 1.2
+eps2= 1.2
+alpha1= 1
+alpha2= 1
+
+xs = alpha1*math.cos(ni)**eps1*math.cos(omega)**eps2
+ys = alpha2*math.cos(ni)**eps1*math.sin(omega)**eps2
+zs = alpha3*sin(ni)**eps2
+Xtest = np.column_stack([xs.ravel(),ys.ravel()])
+
+"""
+# Define the kernel
+def kernel(a, b):
+    """ GP squared exponential kernel """
+    kernelParameter = 0.1
+    sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
+    return np.exp(-.5 * (1/kernelParameter) * sqdist)
+
+N = 10         # number of training points.
+n = 50         # number of test points.
+s = 0.00005    # noise variance.
+
+# Sample some input points and noisy versions of the function evaluated at
+# these points. 
+X = np.random.uniform(-5, 5, size=(N,1))
+y = f(X) + s*np.random.randn(N)
+
+K = kernel(X, X)
+L = np.linalg.cholesky(K + s*np.eye(N))
+
+# points we're going to make predictions at.
+Xtest = np.linspace(-5, 5, n).reshape(-1,1)
+
+# compute the mean at our test points.
+Lk = np.linalg.solve(L, kernel(X, Xtest))
+mu = np.dot(Lk.T, np.linalg.solve(L, y))
+
+# compute the variance at our test points.
+K_ = kernel(Xtest, Xtest)
+s2 = np.diag(K_) - np.sum(Lk**2, axis=0)
+s = np.sqrt(s2)
+
+
+# PLOTS:
+pl.figure(1)
+pl.clf()
+pl.plot(X, y, 'r+', ms=20)
+pl.plot(Xtest, f(Xtest), 'b-')
+pl.gca().fill_between(Xtest.flat, mu-3*s, mu+3*s, color="#dddddd")
+pl.plot(Xtest, mu, 'r--', lw=2)
+pl.savefig('predictive.png', bbox_inches='tight')
+pl.title('Mean predictions plus 3 st.deviations')
+pl.axis([-5, 5, -3, 3])
+
+# draw samples from the prior at our test points.
+L = np.linalg.cholesky(K_ + 1e-6*np.eye(n))
+f_prior = np.dot(L, np.random.normal(size=(n,10)))
+pl.figure(2)
+pl.clf()
+pl.plot(Xtest, f_prior)
+pl.title('Ten samples from the GP prior')
+pl.axis([-5, 5, -3, 3])
+pl.savefig('prior.png', bbox_inches='tight')
+
+# draw samples from the posterior at our test points.
+L = np.linalg.cholesky(K_ + 1e-6*np.eye(n) - np.dot(Lk.T, Lk))
+f_post = mu.reshape(-1,1) + np.dot(L, np.random.normal(size=(n,10)))
+"""
+pl.figure(3)
+pl.clf()
+pl.plot(Xtest, f_post)
+pl.title('Ten samples from the GP posterior')
+pl.axis([-5, 5, -3, 3])
+pl.savefig('post.png', bbox_inches='tight')
+
+pl.show()
+
