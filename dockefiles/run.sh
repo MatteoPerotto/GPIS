690103f9eff5b6d93996727b06a2410cf17ec33a
Matteo Perotto <perotto.matteo@gmail.com>
2022-03-08 13:08:27 +0100

    First test code

---
+++ dockefiles/run.sh
@@ -0,0 +1,9 @@
+# ====================================
+# Run commands for deveopement 
+# ====================================
+docker run \
+    -it \
+    --gpus all \
+    --name $1\
+    ubuntu:gpis
+
