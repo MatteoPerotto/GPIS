9151194117edcace4c0ab5688a6942f08918cd2f
MatteoPerotto <perotto.matteo@gmail.com>
2022-03-09 18:42:24 +0100

    Added code for R2 points

--- gp3D.py
+++ gp3D.py
@@ -1,87 +1,84 @@
 from __future__ import division
 import numpy as np
-import matplotlib.pyplot as pl
-import math 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import sklearn.metrics
 
 
-# Define a superquadric
-N = 20
-ni = np.linspace(-math.pi()/2, math.pi()/2, N)
-omega = np.linspace(-math.pi(), math.pi(), N)
+dlim = 5
+nTrX, nTrY = (50, 50)
+nTsX, nTsY = (20, 20)
+l = 3
+varS = 0.1
+varN = 4
 
-eps1= 1.2
-eps2= 1.2
-alpha1= 1
-alpha2= 1
+xTst, yTst = np.meshgrid(np.linspace(-dlim, dlim, nTsX),np.linspace(-dlim, dlim, nTsY))
+x = xTst.ravel()
+y = yTst.ravel()
+testX = np.column_stack([x,y])
+print("Test: ", testX.shape)
 
-xs = alpha1*math.cos(ni)**eps1*math.cos(omega)**eps2
-ys = alpha2*math.cos(ni)**eps1*math.sin(omega)**eps2
-zs = alpha3*sin(ni)**eps2
-Xtest = np.column_stack([xs.ravel(),ys.ravel()])
-
-"""
-# Define the kernel
-def kernel(a, b):
-    kernelParameter = 0.1
-    sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
-    return np.exp(-.5 * (1/kernelParameter) * sqdist)
-
-N = 10         # number of training points.
-n = 50         # number of test points.
-s = 0.00005    # noise variance.
-
-# Sample some input points and noisy versions of the function evaluated at
-# these points. 
-X = np.random.uniform(-5, 5, size=(N,1))
-y = f(X) + s*np.random.randn(N)
-
-K = kernel(X, X)
-L = np.linalg.cholesky(K + s*np.eye(N))
-
-# points we're going to make predictions at.
-Xtest = np.linspace(-5, 5, n).reshape(-1,1)
-
-# compute the mean at our test points.
-Lk = np.linalg.solve(L, kernel(X, Xtest))
-mu = np.dot(Lk.T, np.linalg.solve(L, y))
-
-# compute the variance at our test points.
-K_ = kernel(Xtest, Xtest)
-s2 = np.diag(K_) - np.sum(Lk**2, axis=0)
-s = np.sqrt(s2)
+xTr, yTr = np.meshgrid(np.linspace(-dlim, dlim, nTrX),np.linspace(-dlim, dlim, nTrY))
+x = xTr.ravel()
+y = yTr.ravel()
+trainX = np.column_stack([x,y])
+trainY = np.sin(np.sqrt(x**2+y**2))
+print("trainX:", trainX.shape)
+print("trainY:", trainY.shape)
 
 
-# PLOTS:
-pl.figure(1)
-pl.clf()
-pl.plot(X, y, 'r+', ms=20)
-pl.plot(Xtest, f(Xtest), 'b-')
-pl.gca().fill_between(Xtest.flat, mu-3*s, mu+3*s, color="#dddddd")
-pl.plot(Xtest, mu, 'r--', lw=2)
-pl.savefig('predictive.png', bbox_inches='tight')
-pl.title('Mean predictions plus 3 st.deviations')
-pl.axis([-5, 5, -3, 3])
+# Define the kernel for R2 points
+def kernel(X, l=1, sig_var=1):
+    if isinstance(X, list):
+        distance = sklearn.metrics.pairwise_distances(*X)
+    else:
+        distance = sklearn.metrics.pairwise_distances(X)
+    return sig_var*np.exp(-1/(2*l**2)*distance**2)
 
-# draw samples from the prior at our test points.
-L = np.linalg.cholesky(K_ + 1e-6*np.eye(n))
-f_prior = np.dot(L, np.random.normal(size=(n,10)))
-pl.figure(2)
-pl.clf()
-pl.plot(Xtest, f_prior)
-pl.title('Ten samples from the GP prior')
-pl.axis([-5, 5, -3, 3])
-pl.savefig('prior.png', bbox_inches='tight')
+def kernelP(X, l=1, sig_var=1):
+    p = 2
+    if isinstance(X, list):
+        distance = sklearn.metrics.pairwise_distances(*X)
+    else:
+        distance = sklearn.metrics.pairwise_distances(X)
+    return sig_var*np.exp(-(2*np.sin(np.pi*distance/p)**2)/(l**2))
 
-# draw samples from the posterior at our test points.
-L = np.linalg.cholesky(K_ + 1e-6*np.eye(n) - np.dot(Lk.T, Lk))
-f_post = mu.reshape(-1,1) + np.dot(L, np.random.normal(size=(n,10)))
-"""
-pl.figure(3)
-pl.clf()
-pl.plot(Xtest, f_post)
-pl.title('Ten samples from the GP posterior')
-pl.axis([-5, 5, -3, 3])
-pl.savefig('post.png', bbox_inches='tight')
 
-pl.show()
+def GP3D(trainX, trainY, testX, l=1, sig_var=1, noise_var=1):
+    Kss = kernel(testX, l, sig_var)
+    K = kernel(trainX, l, sig_var)
+    L = np.linalg.cholesky(K + noise_var*np.eye(len(trainY)))
+    K_s = kernel([trainX, testX], l, sig_var)
+    alpha = np.linalg.solve(L.T, np.linalg.solve(L, trainY))
+    mu = (K_s.T@alpha).squeeze()
+    v = np.linalg.solve(L, K_s)
+    cov =  Kss - v.T@v
+    #stdv = np.sqrt(np.diag(cov)).squeeze()
+    return mu, cov
 
+mu, cov = GP3D(trainX, trainY, testX, l, varS, varN)
+
+
+fig3D = plt.figure(figsize=plt.figaspect(1))  # Square figure
+ax = fig3D.gca(projection='3d')
+ax.scatter(xTr, yTr, trainY.reshape(nTrX,nTrY), color='b')
+ax.scatter(xTst, yTst, mu.reshape(nTsX,nTsY), color='r')
+for axis in 'xyz':
+    getattr(ax, 'set_{}lim'.format(axis))((-dlim, dlim))
+
+getattr(ax, 'set_{}lim'.format('z'))((-1, 1))
+plt.show()
+fig3D.savefig('3D.png')
+
+
+halfTrain = int(nTrX*nTrX/2)
+halfTest = int(nTsX*nTsX/2)
+fig2D = plt.figure(figsize=plt.figaspect(1))  # Square figure
+plt.scatter(xTst.ravel()[halfTest:halfTest+nTsX], mu[halfTest:halfTest+nTsX], color='b')
+plt.scatter(trainX[halfTrain:halfTrain+nTrX,0], trainY[halfTrain:halfTrain+nTrX], color='r')
+plt.show()
+fig2D.savefig('2D.png')
+
+
+
+ 
